df02662e97f22395c25589e8d146644602f14a29
Majer.Marton <Majer.Marton@C214-06>
2026-09-09 08:36:11 +0200

    Add project files.

---
+++ Book/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>
+            {
+                new Book("J.K. Rowling", "Harry Potter and the Sorcerer's Stone", 309),
+                new Book("George Orwell", "1984", 328),
+                new Book("J.R.R. Tolkien", "The Hobbit", 310)
+            };
+        }
+    }
+}
---
+++ Book/book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    internal class Book
+    {
+        public string author {  get; set; }
+        public string title { get; set; }
+        private int pagecount { get; set; }
+
+        public Book(string Author,string Title, int Pagecount)
+        { 
+            author = Author;
+            title = Title;
+            pagecount = Pagecount;
+        }
+        public override string ToString()
+        {
+            return $"Author: {author}, Title: {title}, Page Count: {pagecount}";
+        }
+
+    }
+}
